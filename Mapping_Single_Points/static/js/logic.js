272553ee9be1ce96933ade2417c3def82321154b
JemiShieh <jemishieh@hotmail.com>
2022-01-11 01:02:33 -0500

    Updating logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
